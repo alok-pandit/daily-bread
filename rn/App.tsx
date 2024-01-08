649081a3c04b61413d3c00143f0e05350247c4ab
Alok Pandit <alokpandit@outlook.com>
2024-01-09 04:47:12 +0530

    react navigation added

--- rn/App.tsx
+++ rn/App.tsx
@@ -1,12 +1,13 @@
-import { SafeAreaView, ScrollView, StatusBar, Text } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { SafeAreaView } from 'react-native'
 
+import Routes from './src/routes'
 const App = () => {
   return (
     <SafeAreaView className="bg-lime-400 flex-1 items-center justify-center">
-      <StatusBar barStyle={'dark-content'} backgroundColor={'transparent'} />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Text>Yo</Text>
-      </ScrollView>
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
     </SafeAreaView>
   )
 }
---
+++ rn/src/routes/index.tsx
@@ -0,0 +1,7 @@
+import { Text } from 'react-native'
+
+const Routes = () => {
+  return <Text>Routes</Text>
+}
+
+export default Routes
