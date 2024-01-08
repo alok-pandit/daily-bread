5356f6d10539e0920b6d48a63cc7df311bb92053
Alok Pandit <alokpandit@outlook.com>
2024-01-09 04:35:10 +0530

    eslint and turbo added

--- rn/App.tsx
+++ rn/App.tsx
@@ -1,26 +1,14 @@
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  Text,
-  useColorScheme
-} from 'react-native';
+import { SafeAreaView, ScrollView, StatusBar, Text } from 'react-native'
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-  
   return (
-    <SafeAreaView className='bg-lime-400 flex-1 items-center justify-center'>
-      <StatusBar
-        barStyle={'dark-content'}
-        backgroundColor={'transparent'}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic">
-          <Text>Yo</Text>
+    <SafeAreaView className="bg-lime-400 flex-1 items-center justify-center">
+      <StatusBar barStyle={'dark-content'} backgroundColor={'transparent'} />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <Text>Yo</Text>
       </ScrollView>
     </SafeAreaView>
-  );
-};
+  )
+}
 
-export default App;
+export default App
